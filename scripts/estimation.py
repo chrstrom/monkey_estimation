4613b091d29c804c57db3c0bf6fac08b81c46ad4
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-05 19:36:56 +0200

    Add functions from the now removed fast_dtft
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- scripts/estimation.py
+++ scripts/estimation.py
@@ -2,35 +2,56 @@
 
 import Signals
 import cfg
+from scipy import fft, fftpack
 
 from math import atan2
 from cmath import exp, pi
 
 import numpy as np
 
-class Estimators:
-  def __init__(self, M=None):
+class FFTEstimator:
+  def __init__(self, M):
     # Init the class with parameters from config-file
     self.N = cfg.N
     self.T = cfg.Ts
     self.n0 = cfg.n0
+    self.Fs = cfg.Fs
 
-    if M is None:
-      self.M = cfg.M
-    else:
-      self.M = M
+    self.M = M
+
+
+  def fast_dtft(self, signal):    
+    Fx = fft(signal, self.M)
+    Ff = fftpack.fftfreq(self.M, 1 / self.Fs)
+
+    Fx = fftpack.fftshift(Fx)
+    Ff = fftpack.fftshift(Ff)
+    
+    return Fx, Ff
+
+
+  def magnitude(self, signal):
+    # Return || signal ||_{2} ^ 2
+    magnitude = np.zeros(len(signal))
+    
+    for i in range(len(signal)):
+      magnitude[i] = abs(signal[i])
+
+    return magnitude
+
 
   def calculate_m_star(self, signal):
     # Finds the index with the maximum magnitude
     m = np.argmax(signal)
 
     # Change m to counteract the shift in FFT
-    if m > self.N:
-      m = m - self.N
+    if m > self.M/2:
+      m = m - self.M/2
     else:
-      m = self.N - m
+      m = self.M/2 - m
 
     return m
+
 
   def F_omega0(self, x_d, omega0):
     # Calculates the fourier and normalizes it
@@ -41,15 +62,21 @@ class Estimators:
     
     return complex_sum / self.N
 
-  def estimate_omega(self, x_fft):
-    m_star = self.calculate_m_star(x_fft)
+
+  def estimate_omega(self, x_d):
+    # Estimates the signals angular frequency
+    x_f, _ = self.fast_dtft(x_d)
+    x_mag = self.magnitude(x_f)
+
+    m_star = self.calculate_m_star(x_mag)
 
     omega_hat = 2 * pi * m_star / (self.M * self.T)
     return omega_hat
 
-  def estimate_phase(self, x_d, x_fft):
+
+  def estimate_phase(self, x_d):
     # Estimates the phase of the signal
-    omega_estimate = self.estimate_omega(x_fft)
+    omega_estimate = self.estimate_omega(x_d)
 
     F_omega_estimate = self.F_omega0(x_d, omega_estimate)
 
@@ -60,19 +87,3 @@ class Estimators:
       phi_hat += pi
       
     return phi_hat
-
-#if __name__ == '__main__':
-#  est = Estimators()
-#  sig = Signals.Signals()
-#
-#  signal = sig.x_discrete()
-#
-#  for i in range(len(signal)):
-#    if signal[i] == complex(0,0):
-#      print("May be zero-padding error at index ", i)
-#
-#  omega_estimate = est.estimate_omega(signal)
-#  phase_estimate = est.estimate_phase(signal)
-#
-#  print("True omega: {}, estimated omega: {}".format(cfg.f0*2*pi, omega_estimate))
-#  print("True phase: {}, estimated phase: {}".format(cfg.phi, phase_estimate))
