9e57b5220e1360c0ffffd9166265d32e68e84f6f
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-07 23:28:11 +0200

    QoL updates
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

--- solocarry_estimation.py
+++ solocarry_estimation.py
@@ -11,29 +11,25 @@ def generate_crlb(sigma_squared):
     return crlb_w, crlb_phi
 
 def generate_signal(sigma_squared):
+    # Noise generation
     wr = np.random.normal(0, sigma_squared, N)
     wi = np.random.normal(0, sigma_squared, N)
     w = wr + 1j*wi
 
+    # Pure signal generation
     x = np.empty(N, dtype=np.complex_)
     for n in range(N):
-        x[n] = A*np.exp(1j*(w0*(n+n0)*T + phi)) + w[n]
+        x[n] = A*np.exp(1j*(w0*(n+n0)*T + phi))
 
-    return x
+    return x + w
 
 def fft_estimator(x_d, M):
-    Fx = np.fft.fft(x_d, M)
+    Fw = np.fft.fft(x_d, M)
 
-    m_star = np.argmax(np.absolute(Fx))
-    w_hat = 2*np.pi*m_star / (M*T)
+    m_star = np.argmax(np.absolute(Fw))
 
-    F_w_hat = 0
-    for n in range(N):
-        F_w_hat += x_d[n]*np.exp(-1j*w_hat*n*T)
-    F_w_hat /= N
-
-    phi_arg = np.exp(-1j*w_hat*n0*T)*F_w_hat
-    phi_hat = np.angle(phi_arg)
+    w_hat = 2*np.pi*m_star / (M*T)                          # Eq. (8)
+    phi_hat = np.angle(np.exp(-1j*w_hat*n0*T)*Fw[m_star])   # Eq. (7)
 
     return w_hat, phi_hat
 
@@ -56,7 +52,7 @@ Q = N*(N-1)*(2*N-1)/6.0
 n0 = -P/N
 
 # Generate multiple samples to calculate variance
-SNR_dB = 0.0
+SNR_dB = -10.0
 SNR = 10**(SNR_dB/10.0)
 
 K = 10
@@ -65,10 +61,8 @@ M = 2**K
 sigma_squared = A**2 / (2*SNR)
 
 NUM = 100
-
 w_estimates = np.empty(NUM)
 phi_estimates = np.empty(NUM)
-
 for i in range(NUM):
 
     x_d = generate_signal(sigma_squared)
@@ -77,10 +71,14 @@ for i in range(NUM):
     w_estimates[i] = w_hat
     phi_estimates[i] = phi_hat
     
+mean_w = np.mean(w_estimates)
+mean_phi = np.mean(phi_estimates)
+
 var_w = np.var(w_estimates)
 var_phi = np.var(phi_estimates)
+
 crlb_w, crlb_phi = generate_crlb(sigma_squared)
 
 print("SNR [dB]: {}, M: 2^{}".format(SNR_dB, K))
-print("OMEGA | estimated variance: {}, crlb: {}".format(var_w, crlb_w))
-print("PHASE | estimated variance: {}, crlb: {}".format(var_phi, crlb_phi))
+print("OMEGA | estimated mean: {}, estimated variance: {}, crlb: {}".format(mean_w, var_w, crlb_w))
+print("PHASE | estimated mean: {}, estimated variance: {}, crlb: {}".format(mean_phi, var_phi, crlb_phi))
