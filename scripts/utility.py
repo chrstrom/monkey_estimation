525f6190da587a633787dd0ae07aff5c490f384e
Christopher Strøm <chstr@stud.ntnu.no>
2021-04-08 20:05:33 +0200

    Add status bar utility
    Signed-off-by: Christopher Strøm <chstr@stud.ntnu.no>

---
+++ scripts/utility.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import sys
+
+def print_status_bar(i, progress, N, size=50):
+    if i % (N/size) == 0:
+        progress += 1
+
+    sys.stdout.write('\r')
+    sys.stdout.write("Status: [" + "="*progress + " "*(size-progress) +  "]")
+    sys.stdout.flush()
+
+    return progress
