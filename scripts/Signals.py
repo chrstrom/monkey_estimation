5ec19e648d90ef7b729ba18afa0ee50977fe8aa6
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-04-23 18:37:41 +0200

    Update from master and add some comments
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- scripts/Signals.py
+++ scripts/Signals.py
@@ -6,76 +6,69 @@ import matplotlib.pyplot as plt
 
 import cfg
 
-def sigma_squared_from_SNR(A, SNR):
-    return A**2 / (2*float(SNR))
+def sigma_squared_from_SNR_dB(SNR_dB):
+    """
+    Calculate the value for sigma^2 according to the
+    definition.
+    """
+    SNR = 10**(SNR_dB/10.0)
+    return cfg.A**2 / (2*float(SNR)) # Float casting prevents floor division
 
-class Signals:
+def F(x_d, w):
+    """
+    Calculate F(w) according to Eq. (6) in the project
+    specifications.
+    """
+    sum = 0
+    for n in range(0, cfg.N):
+        sum += x_d[n]*np.exp(-1j*w*n*cfg.Ts)
 
-    def __init__(self, SNR_dB=None):
-        self.N   = cfg.N
-        self.Fs  = cfg.Fs
-        self.Ts  = cfg.Ts
-        self.A   = cfg.A
+    return sum / cfg.N
 
-        self.P   = cfg.P
-        self.Q   = cfg.Q
-        self.n0  = cfg.n0
+def x_discrete(SNR_dB):
+    """
+    Generate a signal according to the problem spec.
+    which consists of a complex exponential with 
+    added noise. Noise-to-signal ratio defined by SNR
+    """
+    sigma = np.sqrt(sigma_squared_from_SNR_dB(SNR_dB))
 
-        self.phi = cfg.phi
+    wr = np.random.normal(0, sigma, cfg.N)
+    wi = np.random.normal(0, sigma, cfg.N)
+    w = wr + 1j*wi
 
-        self.f0  = cfg.f0
-        self.w0  = cfg.w0
+    x = np.empty(cfg.N, dtype=np.complex_)
 
-        if SNR_dB is None:
-            self.SNR = cfg.SNR
-        else:
-            self.SNR = 10**(SNR_dB/10.0)
+    for n in range(cfg.N):
+        z = 1j*(cfg.w0 * (n+cfg.n0) * cfg.Ts + cfg.phi)
+        x[n] = cfg.A * np.exp(z)
 
-        self.sigma = sigma_squared_from_SNR(self.A, self.SNR)
-
-    def F(self, w0):
-        x = self.x_discrete()
-        sum = 0
-        for n in range(0, self.N):
-            sum += x[n]*exp(-1j*w0*n*self.Ts)
-
-        return sum / self.N
-
-    def x_discrete(self):
-        # Generate the data for the sampled signal
-        x = [0 for i in range(self.N)]
-        noise_real = np.random.normal(0, self.sigma, self.N)
-        noise_imag = np.random.normal(0, self.sigma, self.N)
-
-        n = self.n0
-        for i in range(self.N):
-            z = complex(0, self.w0 * n * self.Ts + self.phi)
-            x[i] = self.A * exp(z) + complex(noise_real[i], noise_imag[i])
-            n += 1
-
-        return x
-
-    def x_frequency(self, frequency):
-        # Generates a theoretical signal for a given frequency
-        x = [0 for i in range(self.N)]
-        
-        n = self.n0
-        for i in range(self.N):
-            z = complex(0, 2 * pi * frequency * n * self.Ts + self.phi)
-            x[i] = self.A * exp(z)
-            n += 1
-
-        return x
+    return x + w
 
 
-    def x_phase(self, phase):
-        # Generates a theoretical signal without noise
-        x = [0 for i in range(self.N)]
-        
-        n = self.n0
-        for i in range(self.N):
-            z = complex(0, self.w0 * n * self.Ts + phase)
-            x[i] = self.A * exp(z)
-            n += 1
+def x_frequency(frequency):
+    """ 
+    Generates a complex-exponential signal with given frequency
+    and without noise
+    """
+    x = np.empty(cfg.N, dtype=np.complex_)
+    
+    for n in range(cfg.N):
+        z = 1j*(2 * np.pi * frequency * (cfg.n0+n) * cfg.Ts + cfg.phi)
+        x[n] = cfg.A * np.exp(z)
 
-        return x
+    return x
+
+
+def x_phase(phase):
+    """ 
+    Generates a complex-exponential signal with given phase
+    and without noise
+    """
+    x = np.empty(cfg.N, dtype=np.complex_)
+
+    for n in range(cfg.N):
+        z = 1j*(cfg.w0 * (cfg.n0+n) * cfg.Ts + phase)
+        x[n] = cfg.A * np.exp(z)
+
+    return x
