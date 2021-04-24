833060305ab9d54053ce96f77c86becabaff4aa3
Øystein Solbø <oyssolbo@stud.ntnu.no>
2021-04-24 19:08:59 +0200

    Changed signals to create a random signal based on freuqency and phase
    Signed-off-by: Øystein Solbø <oyssolbo@stud.ntnu.no>

--- scripts/signals.py
+++ scripts/signals.py
@@ -49,29 +49,15 @@ def x_discrete(SNR_dB):
     return x + w
 
 
-def x_frequency(frequency):
+def x_ideal(frequency, phase):
     """ 
     Generates a complex-exponential signal with given frequency
-    and without noise
+    and phase. Does not contain noise
     """
     x = np.empty(cfg.N, dtype=np.complex_)
     
     for n in range(cfg.N):
-        z = 1j*(2 * np.pi * frequency * (cfg.n0+n) * cfg.Ts + cfg.phi)
-        x[n] = cfg.A * np.exp(z)
-
-    return x
-
-
-def x_phase(phase):
-    """ 
-    Generates a complex-exponential signal with given phase
-    and without noise
-    """
-    x = np.empty(cfg.N, dtype=np.complex_)
-
-    for n in range(cfg.N):
-        z = 1j*(cfg.w0 * (cfg.n0+n) * cfg.Ts + phase)
+        z = 1j*(2 * np.pi * frequency * (cfg.n0+n) * cfg.Ts + phase)
         x[n] = cfg.A * np.exp(z)
 
     return x
